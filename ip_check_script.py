0459e2ba69c437031a4ad20eda41aaf1c02ee7c7
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-19 16:41:33 +0800

    Update ip_check_script.py

--- ip_check_script.py
+++ ip_check_script.py
@@ -1,7 +1,6 @@
-import subprocess
+import requests
 import ipaddress
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import threading
 
 def get_subnets_24(subnet):
     """Convert a subnet to multiple /24 subnets."""
@@ -9,25 +8,17 @@ def get_subnets_24(subnet):
     return list(network.subnets(new_prefix=24))
 
 def is_ip_reachable(ip):
-    result = subprocess.run(["ping", "-c", "1", "-W", "1", str(ip)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return result.returncode == 0
+    try:
+        response = requests.get(f'http://{ip}/cdn-cgi/trace', timeout=2)
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
 
 def first_reachable_ip_in_subnet(subnet):
     for ip in ipaddress.ip_network(subnet, strict=False).hosts():
         if is_ip_reachable(ip):
             return ip
     return None
-
-counter = 0
-counter_lock = threading.Lock()
-
-def worker_function(subnet):
-    global counter
-    result = first_reachable_ip_in_subnet(subnet)
-    with counter_lock:
-        counter += 1
-        print(f"Progress: {counter}/{total} subnets checked")
-    return result
 
 if __name__ == '__main__':
     with open('ip.txt', 'r') as file:
@@ -41,13 +32,17 @@ if __name__ == '__main__':
 
     reachable_ips = []
     total = len(all_subnets_24)
+    completed = 0
 
     with ThreadPoolExecutor(max_workers=50) as executor:
-        results = list(executor.map(worker_function, sorted(all_subnets_24)))
-
-    for ip in results:
-        if ip:
-            reachable_ips.append(ip)
+        future_to_subnet = {executor.submit(first_reachable_ip_in_subnet, subnet): subnet for subnet in sorted(all_subnets_24)}
+        for future in as_completed(future_to_subnet):
+            completed += 1
+            subnet = future_to_subnet[future]
+            result = future.result()
+            if result:
+                reachable_ips.append(result)
+            print(f"Progress: {completed}/{total} subnets checked")
 
     with open('reachable_ips.txt', 'w') as file:
         for ip in reachable_ips:

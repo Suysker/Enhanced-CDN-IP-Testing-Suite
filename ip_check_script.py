841e337065e322f7bd87b5a82de21ffee344db2b
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-19 15:43:41 +0800

    Create ip_check_script.py

---
+++ ip_check_script.py
@@ -0,0 +1,34 @@
+import subprocess
+import ipaddress
+
+def get_subnets_24(subnet):
+    network = ipaddress.ip_network(subnet, strict=False)
+    for subnet in network.subnets(new_prefix=24):
+        yield subnet
+
+def is_ip_reachable(ip):
+    # 使用tcping命令来检查IP的可达性，需要确保tcping已经安装
+    result = subprocess.run(["tcping", "-t", "1", str(ip)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.returncode == 0
+
+if __name__ == '__main__':
+    with open('ip.txt', 'r') as file:
+        subnets = file.readlines()
+
+    all_subnets_24 = []
+    for subnet in subnets:
+        subnet = subnet.strip()
+        for new_subnet in get_subnets_24(subnet):
+            all_subnets_24.append(new_subnet)
+
+    reachable_ips = []
+    for subnet in sorted(all_subnets_24):
+        ip = ipaddress.ip_network(subnet, strict=False).network_address
+        if is_ip_reachable(ip):
+            reachable_ips.append(subnet)
+
+    with open('reachable_ips.txt', 'w') as file:
+        for ip in reachable_ips:
+            file.write(str(ip) + '\n')
+
+    print("Done!")

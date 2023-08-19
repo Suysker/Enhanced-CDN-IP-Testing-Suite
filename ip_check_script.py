679ff6cb6244f72f3e425da64aae4f75a8cd8a84
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-19 16:19:40 +0800

    Update ip_check_script.py

--- ip_check_script.py
+++ ip_check_script.py
@@ -8,8 +8,8 @@ def get_subnets_24(subnet):
     return list(network.subnets(new_prefix=24))
 
 def is_ip_reachable(ip):
-    result = subprocess.run(["tcptraceroute", "-f", "255", "-m", "255", "-q", "1", str(ip)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return "Destination unreachable" not in result.stdout.decode()
+    result = subprocess.run(["ping", "-c", "1", "-W", "1", str(ip)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.returncode == 0
 
 def first_reachable_ip_in_subnet(subnet):
     for ip in ipaddress.ip_network(subnet, strict=False).hosts():
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     total = len(all_subnets_24)
     completed = 0
 
-    with ThreadPoolExecutor(max_workers=128) as executor:
+    with ThreadPoolExecutor(max_workers=50) as executor:
         future_to_subnet = {executor.submit(first_reachable_ip_in_subnet, subnet): subnet for subnet in sorted(all_subnets_24)}
         for future in as_completed(future_to_subnet):
             completed += 1

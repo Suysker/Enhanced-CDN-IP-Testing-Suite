47296d763d15ce2afdaf2d77a4857a825300578a
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-19 15:50:59 +0800

    Update ip_check_script.py

--- ip_check_script.py
+++ ip_check_script.py
@@ -1,5 +1,6 @@
 import subprocess
 import ipaddress
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def get_subnets_24(subnet):
     network = ipaddress.ip_network(subnet, strict=False)
@@ -22,10 +23,18 @@ if __name__ == '__main__':
             all_subnets_24.append(new_subnet)
 
     reachable_ips = []
-    for subnet in sorted(all_subnets_24):
-        ip = ipaddress.ip_network(subnet, strict=False).network_address
-        if is_ip_reachable(ip):
-            reachable_ips.append(subnet)
+    total = len(all_subnets_24)
+    completed = 0
+
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        future_to_subnet = {executor.submit(check_subnet, subnet): subnet for subnet in sorted(all_subnets_24)}
+        for future in as_completed(future_to_subnet):
+            completed += 1
+            subnet = future_to_subnet[future]
+            result = future.result()
+            if result:
+                reachable_ips.append(result)
+            print(f"Progress: {completed}/{total} subnets checked")
 
     with open('reachable_ips.txt', 'w') as file:
         for ip in reachable_ips:

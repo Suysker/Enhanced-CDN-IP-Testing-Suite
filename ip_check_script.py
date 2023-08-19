2b93a45c0ee3d03c5ffbd377070490d4cfc18045
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-19 21:12:50 +0800

    Update ip_check_script.py

--- ip_check_script.py
+++ ip_check_script.py
@@ -5,27 +5,30 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 urlprefix = ".ip.suyskser.top"
 
 def get_subnets_24(subnet):
+    """Convert a subnet to multiple /24 subnets."""
     network = ipaddress.ip_network(subnet, strict=False)
     return list(network.subnets(new_prefix=24))
 
-def get_colo_from_trace(ip):
+def get_colo_if_reachable(ip):
     try:
         result = subprocess.run(["curl", "-s", f"http://{ip}/cdn-cgi/trace"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=2)
-        for line in result.stdout.decode().split("\n"):
-            if line.startswith("colo="):
-                return line.split("=")[1]
-        return None
+        response = result.stdout.decode().strip()
+        
+        if "200" in response:  # 基于HTTP 200状态码检查IP是否可访问
+            for line in response.split("\n"):
+                if line.startswith("colo="):
+                    return ip, line.split("=")[1]
+        return None, None
     except subprocess.TimeoutExpired:
-        return None
+        return None, None
 
 def first_reachable_ip_in_subnet(subnet):
     consecutive_failures = 0
     for i in range(0, 256, 25):
         ip = ipaddress.ip_address(f"{subnet.network_address + i}")
-        if is_ip_reachable(ip):
-            colo = get_colo_from_trace(ip)
-            if colo:
-                return (ip, colo)
+        ip_result, colo = get_colo_if_reachable(ip)
+        if ip_result:
+            return ip_result, colo
         else:
             consecutive_failures += 1
             if consecutive_failures >= 10:
@@ -34,7 +37,7 @@ def first_reachable_ip_in_subnet(subnet):
 
 def generate_domain(ip_address):
     parts = str(ip_address).split(".")
-    parts[-1] = "0"
+    parts[-1] = "0"  # 把最后一个部分替换为 "0"
     return "-".join(parts) + urlprefix
 
 if __name__ == '__main__':
@@ -47,7 +50,7 @@ if __name__ == '__main__':
         for new_subnet in get_subnets_24(subnet):
             all_subnets_24.append(new_subnet)
 
-    reachable_ips_and_colo = {}
+    reachable_ips_colos = {}
     total = len(all_subnets_24)
     completed = 0
 
@@ -57,23 +60,18 @@ if __name__ == '__main__':
             completed += 1
             subnet = future_to_subnet[future]
             ip, colo = future.result()
-            if ip and colo:
-                subnet_22 = ipaddress.ip_network(ip).supernet(new_prefix=22)
-                if subnet_22 not in reachable_ips_and_colo:
-                    reachable_ips_and_colo[subnet_22] = (ip, colo)
-                elif reachable_ips_and_colo[subnet_22][1] != colo:
-                    # Replace if different colo
-                    reachable_ips_and_colo[subnet_22] = (ip, colo)
+            if ip:
+                reachable_ips_colos[ip] = colo
             print(f"Progress: {completed}/{total} subnets checked")
 
     # Save reachable IPs
     with open('reachable_ips.txt', 'w') as file:
-        for ip, _ in reachable_ips_and_colo.values():
-            file.write(str(ip) + '\n')
+        for ip, colo in reachable_ips_colos.items():
+            file.write(f"{ip} - {colo}\n")
     
     # Save corresponding domain names with IPs
     with open('bind_config.txt', 'w') as file:
-        for ip, _ in reachable_ips_and_colo.values():
+        for ip in reachable_ips_colos.keys():
             domain = generate_domain(ip)
             file.write(f"{domain}. 1 IN A {ip}\n")
 

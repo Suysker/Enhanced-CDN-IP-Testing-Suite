b5f54b4662b67435f8743bbf54b16d3f7385381e
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-19 16:02:24 +0800

    Update ip_check_script.py

--- ip_check_script.py
+++ ip_check_script.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     total = len(all_subnets_24)
     completed = 0
 
-    with ThreadPoolExecutor(max_workers=50) as executor:
+    with ThreadPoolExecutor(max_workers=128) as executor:
         future_to_subnet = {executor.submit(check_subnet, subnet): subnet for subnet in sorted(all_subnets_24)}
         for future in as_completed(future_to_subnet):
             completed += 1

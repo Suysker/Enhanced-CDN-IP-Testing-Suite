36932b5c9c524d5675d9ea1ab42b833ee946c577
Suysker <61983976+Suysker@users.noreply.github.com>
2023-08-26 23:16:32 +0800

    Update ipv6_check_script.py

--- CloudFront/ipv6_check_script.py
+++ CloudFront/ipv6_check_script.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     total = len(all_subnets_48)
     completed = 0
 
-    with ThreadPoolExecutor(max_workers=1024) as executor:
+    with ThreadPoolExecutor(max_workers=2048) as executor:
         future_to_subnet = {executor.submit(first_reachable_ip_in_subnet, subnet): subnet for subnet in sorted(all_subnets_48)}
         for future in as_completed(future_to_subnet):
             completed += 1
@@ -81,15 +81,15 @@ if __name__ == '__main__':
 
         # Save selected IPs
     with open('CloudFront/ipv6_geo_reachable_ips.txt', 'w') as file:
-        for ip in geo_reachable_ips:
-            file.write(str(ip) + '\n')
+        for ip, geo_code in geo_reachable_ips:
+            file.write(f"{ip} {geo_code}\n")
 
     selected_ips = []
     while reachable_ips:
         first_ip = reachable_ips.pop(0)
         selected_ips.append(first_ip)
-        subnet_40 = ipaddress.ip_network(first_ip).supernet(new_prefix=40)
-        reachable_ips = [ip for ip in reachable_ips if not ipaddress.ip_network(ip).subnet_of(subnet_40)]
+        subnet_44 = ipaddress.ip_network(first_ip).supernet(new_prefix=44)
+        reachable_ips = [ip for ip in reachable_ips if not ipaddress.ip_network(ip).subnet_of(subnet_44)]
 
     simple_reachable_ips = selected_ips
 
@@ -102,6 +102,6 @@ if __name__ == '__main__':
 
     with open('CloudFront/ipv6_geo_simple_reachable_ips.txt', 'w') as file:
         for ip, geo_code in geo_simple_reachable_ips:
-            file.write(str(ip) + ' ' + geo_code + '\n')
+            file.write(f"{ip} {geo_code}\n")
 
     print("Done!")
